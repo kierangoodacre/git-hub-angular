f21502f575ea2999e48f8aa3656cfc09879526a9
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-04-09 15:23:39 +0100

    created call on to git-hubs api

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -9,4 +9,45 @@ describe('GitUserSearchController', function() {
         $scope: scope
     });
   }));
+
+  it('initializes with an empty search result and term', function(){
+    expect(scope.searchResult).toBeUndefined();
+    expect(scope.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function(){
+
+    var httpBackend;
+
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('Displays search results', function(){
+      scope.searchTerm = 'hello';
+      scope.doSearch();
+      scope.$apply();
+      httpBackend.flush();
+      expect(scope.searchResult.items).toEqual(items);
+    });
+
+  });
 });
